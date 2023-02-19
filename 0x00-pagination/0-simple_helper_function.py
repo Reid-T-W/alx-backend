676ee8eb38a3fa27532b972d1e382c19812268d8
Reid-T-W <rediettadesse100@gmail.com>
2023-02-19 11:55:28 +0300

    pycodestyle requirements satisfied

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Generates start and end index given pagination parameters
-    
+
     Parameters
     page : int
         The page number
@@ -21,4 +21,3 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = page_size * (page - 1)
     end_index = start_index + page_size
     return (start_index, end_index)
-    
