7145c432160890b966120dddbc8a1a046db70f86
Reid-T-W <rediettadesse100@gmail.com>
2023-02-28 14:19:39 +0300

    docsting add to get_locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,6 +18,7 @@ app.config.from_object(Config)
 
 # @babel.localeselector
 def get_locale():
+    """ Get best suited language according to users preference"""
     # if a user is logged in, use the locale from the user settings
     user = getattr(g, 'user', None)
     if user is not None:
