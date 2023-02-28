7d42e7298363f50229a8f074bb2b3b0f0160d9eb
Reid-T-W <rediettadesse100@gmail.com>
2023-02-28 12:45:58 +0300

    Simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Starts a flask web application """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def render_index_html():
+    """ Renders the 0-index.html """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
