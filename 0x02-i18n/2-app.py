f50e2756a7272db7a6b92216d5cdd19bd4b7fdc5
Reid-T-W <rediettadesse100@gmail.com>
2023-02-28 14:56:13 +0300

    pycodestyle reqts satisfied

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,7 +22,9 @@ def get_locale():
     """ Get best suited language according to users preference"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/', strict_slashes=False)
 def render_index_html():
