c66acb12f019109786f35d52eb11cdf5f30bfdf4
Reid-T-W <rediettadesse100@gmail.com>
2023-02-28 14:17:16 +0300

    local selector set to get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Starts a flask web application """
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config():
+    """ Configuration setting for flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+# @babel.localeselector
+def get_locale():
+    # if a user is logged in, use the locale from the user settings
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    # otherwise try to guess the language from the user accept
+    # header the browser transmits.
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/', strict_slashes=False)
+def render_index_html():
+    """ Renders the 2-index.html """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
