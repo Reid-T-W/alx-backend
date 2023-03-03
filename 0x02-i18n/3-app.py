b1dce1e65daee05f8f1036458d72a36abeb19fe3
Reid-T-W <rediettadesse100@gmail.com>
2023-03-03 11:26:54 +0300

    gettext moved to html file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,8 +29,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def render_index_html():
     """ Renders the 3-index.html """
-    return render_template('3-index.html', home_title=gettext("home_title"),
-                           home_header=gettext("home_header"))
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
