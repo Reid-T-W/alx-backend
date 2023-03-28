4948dbb171e146bce0dd800dc2ef771474cdce19
Reid-T-W <rediettadesse100@gmail.com>
2023-03-28 18:32:17 +0300

    await async removed

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -4,9 +4,4 @@ const client = createClient();
 
 client.on('connect', () => console.log('Redis client connected to the server'));
 client.on('error', err => console.log('Redis client not connected to the server:', err));
-
-async function main() {
-    await client.connect();
-}
-
-main()
+client.connect()
